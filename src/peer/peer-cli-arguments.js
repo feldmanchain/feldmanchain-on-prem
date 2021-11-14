345d6ef037dae575fe79cf329bd8fb8ad3d85606
Alan Smithee <ggnore.alan.smithee@gmail.com>
2021-11-14 14:25:25 +0100

    add comment to cli file

--- src/peer/peer-cli-arguments.js
+++ src/peer/peer-cli-arguments.js
@@ -1,3 +1,15 @@
+/*
+  NOTE(Alan):
+
+  Commander effectively turns the program into a cli
+  
+  Every option registered below can be passed when running the script,
+  and will be added to the program.opts() accordingly
+
+  It also makes it possible to run `node {script} -h` to see available options,
+  which is really nice
+*/
+
 import { Command } from "commander"
 
 const program = new Command()
