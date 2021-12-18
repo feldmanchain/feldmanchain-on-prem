bae659e1bafc4d00e28a9532d5cc20050405c5f9
Alan Smithee <ggnore.alan.smithee@gmail.com>
2021-12-18 13:23:48 +0100

    remove dead code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import open from "open"
 import { port } from "./lib/cli-options.js"
 import { createPeer, stopPeer } from "./applications/peer.js"
 import { createWebApp, startWebApp } from "./applications/web-app.js"
-import { createWsApp, startWsApp } from "./applications/ws-app.js"
 
 const peer = await createPeer()
 
-const wsApp = createWsApp()
-startWsApp(wsApp, port + 1, (_t) => {})
-
-const webApp = createWebApp(peer, wsApp)
+const webApp = createWebApp(peer, port)
 startWebApp(webApp, port, () => open(`http://localhost:${port}`))
 
 const onProgramTerminated = async () => {
